4a69b2d8653a63a0c80777a9ffe853ecb86e4b56
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-21 15:49:29 +0100

    Add fix crud

--- routers/posts.js
+++ routers/posts.js
@@ -1,36 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = router;
-
 /* Operazioni CRUD */
 
 /* Index */
 router.get("/", (req, res) => {
-  res.send("Lista dei post");
+  res.json("Lista dei post");
 });
 
 /* Show */
-router.get("/show", (req, res) => {
-  res.send("Visualizza il post");
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json("Visualizza il post" + " " + id);
 });
 
 /* Create */
 router.post("/", (req, res) => {
-  res.send("Crea il post");
+  res.json("Crea il post");
 });
 
 /* Update */
-router.put("/update", (req, res) => {
-  res.send("Modifica completamente il post");
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json("Modifica completamente il post" + " " + id);
 });
 
 /* Modify */
-router.patch("/modify", (req, res) => {
-  res.send("Modifica parzialmente il post");
+router.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json("Modifica parzialmente il post" + " " + id);
 });
 
 /* Delete */
-router.delete("/destroy", (req, res) => {
-  res.send("Cancellazione del post");
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json("Cancellazione del post" + " " + id);
 });
+
+module.exports = router;
