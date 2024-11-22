70c1950fa0ede8d9aea2087e36482aa6c8801f2a
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-22 12:33:25 +0100

    Add Giorno 4 instructions

--- app.js
+++ app.js
@@ -76,6 +76,46 @@ Bonus
     Implementare un filtro di ricerca nella index che mostri solo i post che hanno un determinato Tag
     In Show e Destroy, controllare se il parametro si riferisce ad un post esistente, in caso contrario, rispondere con uno stato 404 e un messaggio d’errore, sempre in formato JSON. */
 
+//! Giorno 4
+
+/* Milestone 1
+Per iniziare, andiamo su Postman e prepariamo una nuova chiamata verso la nostra rotta store.
+
+    Impostiamo il verbo e l’endpoint corretti
+    Selezioniamo il tab body e scegliamo il formato raw e JSON
+    Inseriamo come corpo della nostra request un oggetto che rappresenti un nuovo post
+
+Nota: se vogliamo avere delle immagini, inventiamole pure.
+
+Nota: ricordiamo che non bisogna passare l’id quando si crea una nuova risorsa: 
+sarà il server (con l’aiuto del database) a fornirlo.
+
+Milestone 2
+
+Impostiamo il body-parser per far sì che la nostra app riesca a decifrare il request body.
+
+Poi, all’interno della rotta Store, stampiamo nel terminale i dati in arrivo, grazie a un console.log
+
+Milestone 3
+
+Implementiamo quindi la logica per aggiungere un nuovo post al nostro blog, e prepariamo la risposta adeguata.
+
+Testiamolo con postman.
+
+Milestone 4
+Ripetiamo il procedimento per la rotta di Update, in modo da avere la possibilità 
+di modificare le nostre risorse.
+
+Bonus
+
+    Quelli del giorno prima, se non già fatti
+    Aggiungere un controllo dell'errore nelle rotte che utilizzano un parametro 
+    obbligatorio :id quando la risorsa corrispondente non viene trovata, 
+    rispondere con uno stato 404 e un messaggio d’errore, sempre in formato JSON.
+    
+    Aggiungere un controllo dell'errore per i parametri in ingresso nel body nelle rotte store e update , 
+    rispondere con uno stato 400 e un messaggio d’errore, sempre in formato JSON. */
+
 /* Importo express */
 const express = require("express");
 const app = express();
